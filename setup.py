3357578fd52c85b817c1a43bebf71d1d03ce8954
Oshim Pathan <oshimkhan@example.com>
2025-10-25 15:27:59 +0530

    Folder Structure

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Generative AI Project',
-    version= '0.0.0',
-    author= 'Oshim Pathan',
-    author_email= 'work.oshimkhan@gmail.com',
-    packages= find_packages(),
-    install_requires = []
-
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Oseem Pathan",
+    author_email="work.oshimkhan@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
 )
